106a7bae6ddc08052b60d6108b769ac5ac1e4632
adndamelia22-dotcom <adndamelia22@gmail.com>
2026-04-15 22:10:31 +0700

    update mongodb connection

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,1 +1,38 @@
-// mongodb connection setup
+import mongoose from "mongoose";
+
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error("MONGODB_URI belum diisi di .env.local");
+}
+
+const MONGODB_URI: string = uri;
+
+type MongooseCache = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
+
+declare global {
+  var mongooseCache: MongooseCache | undefined;
+}
+
+const cached = global.mongooseCache || {
+  conn: null,
+  promise: null,
+};
+
+global.mongooseCache = cached;
+
+export async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
