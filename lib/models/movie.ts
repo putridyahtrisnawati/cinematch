cd88a1b43b508b44067291d2d54ded357923c1bc
adndamelia22-dotcom <adndamelia22@gmail.com>
2026-04-24 21:03:25 +0700

    feat: add movie descriptions

--- lib/models/movie.ts
+++ lib/models/movie.ts
@@ -5,8 +5,9 @@ const MovieSchema = new mongoose.Schema({
   genre: String,
   rating: Number,
   image: String,
-  desc: String,
+  desc: String,  // teks pendek untuk home bagian coming soon
   status: String,
+  description: String,   // sinopsis untuk detail film
 });
 
 export default mongoose.models.Movie || mongoose.model("Movie", MovieSchema);
